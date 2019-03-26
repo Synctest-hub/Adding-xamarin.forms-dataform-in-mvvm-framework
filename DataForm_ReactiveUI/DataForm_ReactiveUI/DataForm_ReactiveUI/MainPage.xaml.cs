6674c8bc43eab24015f6c9c433c8267335a8a854
Karthik Raja Arumugam <karthikraja.arumugam@syncfusion.com>
2019-03-26 17:31:04 +0530

    DataForm using ReactiveUI

---
+++ DataForm_ReactiveUI/DataForm_ReactiveUI/DataForm_ReactiveUI/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataForm_ReactiveUI
+{
+    public partial class MainPage : ContentPage, IViewFor<ContactInfo>
+    {
+        #region Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new ContactInfo();
+            this.Bind(ViewModel, x => x.AddressInfo, x => x.dataForm.DataObject);
+        }
+        #endregion
+
+        #region ViewModel Setup
+
+        public ContactInfo ViewModel
+        {
+            get;
+            set;
+        }
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (ContactInfo)value; }
+          
+        }
+        #endregion
+    }
+}
---
+++ DataForm_ReactiveUI/DataForm_ReactiveUI/DataForm_ReactiveUI/Model/AddressInfo.cs
@@ -0,0 +1,30 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataForm_ReactiveUI
+{
+    public class AddressInfo
+    {
+        public string Name { get; set; }
+        public string Address{ get; set; }
+    }
+ 
+    public class ContactInfo : ReactiveObject
+    {
+        private AddressInfo addressInfo;
+        public AddressInfo AddressInfo
+        {
+            get => addressInfo;
+            set => this.RaiseAndSetIfChanged(ref addressInfo, value);
+        }
+
+        public ContactInfo()
+        {
+            AddressInfo = new AddressInfo();
+        }
+    }
+}
